51cc866b0d55b6365c1e88868a853310a3812e6f
Vincent <vincent.fischer@hotmail.com>
2024-02-28 14:58:41 +0100

    Moving in the correct folder the training scripts

--- training/random_forest_exploration/RF_training.py
+++ training/random_forest_exploration/RF_training.py
@@ -4,11 +4,10 @@ import re
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
-from imblearn.over_sampling import RandomOverSampler
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.preprocessing import FunctionTransformer
 from imblearn.pipeline import Pipeline
-from sklearn.metrics import fbeta_score
+from imblearn.over_sampling import SMOTE
+from sklearn.metrics import fbeta_score, confusion_matrix
 
 
 # Function to clean the text data
@@ -23,7 +22,7 @@ def clean_text(text):
 
 
 # Load the dataset
-dataset_path = "dataset.csv"
+dataset_path = "./training/dataset.csv"
 data = pd.read_csv(dataset_path, header=0, delimiter=";")
 
 # Add word_count column
@@ -44,36 +43,61 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 
+parameters = {
+    "max_depth": 20,
+    "max_features": "log2",
+    "min_samples_leaf": 1,
+    "min_samples_split": 5,
+    "n_estimators": 300,
+}
 # Define the Random Forest model with specified parameters
 rf_model = RandomForestClassifier(
-    max_depth=20,
-    max_features="log2",
-    min_samples_leaf=1,
-    min_samples_split=5,
-    n_estimators=200,
+    max_depth=parameters["max_depth"],
+    max_features=parameters["max_features"],
+    min_samples_leaf=parameters["min_samples_leaf"],
+    min_samples_split=parameters["min_samples_split"],
+    n_estimators=parameters["n_estimators"],
 )
 
 # Define the TF-IDF vectorizer
 tfidf_vectorizer = TfidfVectorizer(max_features=5000)
 
-# Define the pipeline with data preprocessing, TF-IDF vectorization, RandomOverSampler, and Random Forest Classifier
+X_train = tfidf_vectorizer.fit_transform(X_train)
+X_test = tfidf_vectorizer.transform(X_test)
+
+oversample = SMOTE()
+X_train, y_train = oversample.fit_resample(X_train, y_train)
+
+rf_model.fit(X_train, y_train)
+y_pred = rf_model.predict(X_test)
+
+print("Classification Report:")
+cr = classification_report(y_test, y_pred)
+print(cr)
+
+f2_score = fbeta_score(y_test, y_pred, beta=2)
+print(f"F2-score: {f2_score}")
+
+# Calculate confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion matrix:")
+print(cm)
+
+# Define the pipeline with TF-IDF vectorization and Random Forest Classifier
 pipeline = Pipeline(
     [
-        ("clean_text", FunctionTransformer(clean_text)),
         ("tfidf", tfidf_vectorizer),
-        ("sampling", RandomOverSampler()),
         ("rf", rf_model),
     ]
 )
 
-pipeline.fit(X_train, y_train)
-
-y_pred = pipeline.predict(X_test)
-print("Classification Report:")
-print(classification_report(y_test, y_pred))
-f2 = fbeta_score(y_test, y_pred, beta=2)
-print(f"F2-score: {f2}")
-
-# Once trained, save the model to a pickle file
-with open("trained_rf_pipeline.pkl", "wb") as file:
-    pickle.dump(pipeline, file)
+# Save the model, its score and its parameters
+rf_pipeline = {
+    "pipeline": pipeline,
+    "parameters": parameters,
+    "f2_score": f2_score,
+    "confusion_matrix": cm,
+    "classification_report": cr,
+}
+with open("./models/rf_pipeline.pkl", "wb") as f:
+    pickle.dump(rf_pipeline, f)
